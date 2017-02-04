e45b5f93ba9b119f9b3597565360ac0470501552
Fernando <Fernando>
2017-02-04 16:17:57 -0200

    Http request was implemented

--- GetMovieInfo/src/com/zup/getmovieinfo/GetJsonTask.java
+++ GetMovieInfo/src/com/zup/getmovieinfo/GetJsonTask.java
@@ -1,17 +1,72 @@
 package com.zup.getmovieinfo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 public class GetJsonTask extends AsyncTask<String, String, String> {
 	
+	TextView mText;
+	
+	public GetJsonTask(TextView mText) {
+		this.mText = mText;
+	}
+
 	@Override
 	protected String doInBackground(String... params) {
-		// TODO Auto-generated method stub
+		
+		HttpURLConnection connection = null;
+		BufferedReader bReader = null;
+
+		try {
+			URL url = new URL (params[0]);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.connect();
+			InputStream inStream = connection.getInputStream();
+			bReader = new BufferedReader(new InputStreamReader(inStream));
+			StringBuffer sBuffer = new StringBuffer();
+			String line = "";
+
+			while ((line = bReader.readLine()) != null ) {
+				sBuffer.append(line);
+			}
+		
+			return sBuffer.toString();
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
+			try {
+				if (bReader != null) {
+					bReader.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return null;
 	}
-	
+
 	@Override
-    protected void onProgressUpdate(String... values) {
-		       
-    }    
+	protected void onPostExecute(String result) {
+		super.onPostExecute(result);
+		
+		mText.setText(result);
+		
+	}
 }
--- GetMovieInfo/src/com/zup/getmovieinfo/MainActivity.java
+++ GetMovieInfo/src/com/zup/getmovieinfo/MainActivity.java
@@ -2,12 +2,42 @@ package com.zup.getmovieinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	EditText mEdit;
+	Button mButton;
+	TextView mText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mEdit = (EditText) findViewById(R.id.etSearch);
+		mButton = (Button) findViewById(R.id.btOk);
+		mText = (TextView) findViewById(R.id.tvShowTitle);
+		
+		mButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				String getEdit = mEdit.getText().toString();
+				
+				if (getEdit.contains(" ")) {
+					getEdit = mEdit.getText().toString().replace(" ", "+");
+				} 
+				
+				new GetJsonTask(mText).execute("https://api.myjson.com/bins/tseq9");
+			}
+		});
 	}
 }
