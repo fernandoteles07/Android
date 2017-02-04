fb4c4ef2a43a1a2e92a2b415451e2d5905c0648a
Fernando <Fernando>
2017-02-04 13:34:41 -0200

    GetJsonTask class has been created for handle with HTTP request

---
+++ GetMovieInfo/src/com/zup/getmovieinfo/GetJsonTask.java
@@ -0,0 +1,17 @@
+package com.zup.getmovieinfo;
+
+import android.os.AsyncTask;
+
+public class GetJsonTask extends AsyncTask<String, String, String> {
+	
+	@Override
+	protected String doInBackground(String... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+    protected void onProgressUpdate(String... values) {
+		       
+    }    
+}
